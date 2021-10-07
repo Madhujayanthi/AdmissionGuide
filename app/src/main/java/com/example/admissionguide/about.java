34313c7e4a9263cf2d2c85a7788e7523f50c388e
Lakshmanakash <lakshmanakashnp@gmail.com>
2021-10-07 17:11:33 +0530

    alteration

--- app/src/main/java/com/example/admissionguide/about.java
+++ app/src/main/java/com/example/admissionguide/about.java
@@ -3,12 +3,41 @@ package com.example.admissionguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class about extends AppCompatActivity {
+
+    TextView vision_mission;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_department);
+        setContentView(R.layout.activity_about);
+
+        vision_mission = findViewById(R.id.vision);
+        StringBuilder v = new StringBuilder();
+        String vision="V I S I O N\n" +
+                "\n"+
+                "To become an institute of great repute, in the fields of Science, Engineering, Technology and Management studies, by offering a full range of programmes of global standard to foster research, and to transform the students into globally competent personalities.\n" + "\n"+
+                "M I S S I O N\n" + "\n"+
+                "Sona College of Technology is a private engineering institution that offers engineering degree programmes at under graduate level and post graduate level, computer applications and management studies at post graduate level and doctoral programmes in the areas of engineering and science and humanities.\n" +
+                "\n" +
+                "The college aims to provide a full-fledged education, to produce graduates with competency to excel in the organizations they serve and to cater to the needs of the community as a whole.\n" + "\n"+
+                "Our mission for next three years will be\n" +
+                "\n" +
+                "To offer Graduate, Post-graduate, Doctoral and other value-added programmes beneficial for the students\n" + "\n"+
+                "To establish state-of-the-art facilities and resources required to achieve excellence in teaching-learning, and supplementary processes\n" +  "\n"+
+                "To provide Faculty and Staff with the required qualification and competence and to provide opportunity to upgrade their knowledge and skills\n" +"\n"+
+                "To motivate the students to pursue higher education, appear for competitive exams, and other value added programmes for their holistic development\n" +"\n"+
+                "To provide opportunities to the students and bring out their inherent talent\n" +"\n"+
+                "To establish Centres of excellence in the emerging areas of research\n" +"\n"+
+                "To have regular interaction with the Industries in the area of R & D, and offer consultancy, training and testing services\n" +"\n"+
+                "To offer Continuing education, and Non-formal vocational education programmes that are beneficial to the society\n" +"\n"+
+                "To inculcate entrepreneurial attitude in the students and to provide a platform to start their own startups in the campus.";
+
+        v.append(vision);
+        vision_mission.setText(v.toString());
+
     }
 }
--- app/src/main/java/com/example/admissionguide/admission.java
+++ app/src/main/java/com/example/admissionguide/admission.java
@@ -3,12 +3,53 @@ package com.example.admissionguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class admission extends AppCompatActivity {
+
+    TextView admission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admission);
+
+        admission= findViewById(R.id.admission_text2);
+        StringBuilder admission_string = new StringBuilder();
+        String someMessage ="\n"+
+                "i) Selection Procedure\n" +
+                "\n" +
+                "The selection committee constituted by Sona College of Technology as per the guidelines of the Institution will do selection of the candidates under this scheme. The selection is purely provisional and always subject to the confirmation from the Anna University / Directorate of Technical Education, Chennai.\n" +
+                "\n" +
+                "ii) Selection Committee\n" +
+                "\n" +
+                "The following selection committee constituted by Sona College of Technology as per the guidelines of the Institution will select the candidates for admission for various programmes offered by our institution.\n" +
+                "\n" +
+                "Dr. S.R.R. Senthil Kumar, Principal, Chairman Admissions\n" +
+                "Dr.B.Sathiyabhama, Dean - Admissions\n" +
+                "Mr.Nagappan, In-Charge Admissions\n" +
+                "\n" +
+                "iii) Mode of selection\n" +
+                "\n" +
+                "BE/BTech Degree Programme - First Year\n" +
+                "\n" +
+                "Based on the cut off marks obtained in the higher secondary (10+2) examination.\n" +
+                "Cut off mark = 200 (100 for Mathematics and 100 for Physics + Chemistry)\n" +
+                "BE/BTech Degree Programme - Direct Second Year Lateral Entry\n" +
+                "\n" +
+                "Based on the percentage marks obtained in the Diploma (10+3/12+2) examination.\n" +
+                "ME/MTech Degree Programme\n" +
+                "\n" +
+                "Based on the entrance (TANCET / CET / GATE) marks conducted by the Anna University & Association of Management of Coimbatore Anna University Affiliated Colleges\n" +
+                "Master of Business Administration (MBA) Degree\n" +
+                "\n" +
+                "Based on the entrance (MAT / TANCET / CET) marks conducted by the Anna University & Association of Management of Coimbatore Anna University Affiliated Colleges\n" +
+                "Master of Computer Application (MCA) Degree\n" +
+                "\n" +
+                "Based on the entrance (TANCET / CET) marks conducted by the Anna University & Association of Management of Coimbatore Anna University Affiliated Colleges.";
+
+        admission_string.append(someMessage);
+
+        admission.setText(admission_string.toString());
     }
 }
